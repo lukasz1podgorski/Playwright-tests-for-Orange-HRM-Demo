54b5885d79fdea25d612316876bac415cc0cae1f
Łukasz <podgorski1lukasz@gmail.com>
2024-11-20 21:32:04 +0100

    Added new employee test and edit existing admin user test

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,8 @@ module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+
+  timeout: 60000,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -2,6 +2,17 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login'
 import { AdminPage } from '../pages/admin'
 
+test.describe.configure({ mode: 'serial' });
+
+const username_admin = 'Timothy'
+
+export const random = () => {
+    return (Math.floor(Math.random() * 1000000)).toString();
+}
+const randomNumber = random()
+
+const result = username_admin.concat(randomNumber)
+
 test.beforeEach('login', async ({ page }) => {
 
     const Login = new LoginPage(page)
@@ -13,6 +24,24 @@ test.beforeEach('login', async ({ page }) => {
     
 });
 
+test('Add new employee', async ({page}) => {
+
+    await page.getByRole('link', { name: 'PIM' }).click();
+    await page.getByRole('button', { name: ' Add' }).click();
+    await page.getByPlaceholder('First Name').click();
+    await page.getByPlaceholder('First Name').fill('Timothy');
+    await page.getByPlaceholder('Middle Name').click();
+    await page.getByPlaceholder('Middle Name').fill('Lewis');
+    await page.getByPlaceholder('Last Name').click();
+    await page.getByPlaceholder('Last Name').fill('Amiano');
+    // var timestamp = Date.now().toString()
+    await page.locator('form').getByRole('textbox').nth(4).click()
+    await page.locator('form').getByRole('textbox').nth(4).fill(randomNumber);
+    await page.getByRole('button', { name: 'Save' }).click();
+
+})
+
+
 test('Add new admin user', async ({page}) => {
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers');
@@ -23,13 +52,14 @@ test('Add new admin user', async ({page}) => {
     await page.locator('form i').nth(1).click();
     await page.getByRole('option', { name: 'Enabled' }).click();
     await page.getByPlaceholder('Type for hints...').click();
-    await page.getByPlaceholder('Type for hints...').fill('a');
-    await page.getByText('Timothy Lewis Amiano').click();
+    await page.getByPlaceholder('Type for hints...').fill('Timothy Lewis Amiano');
+    await page.getByText('Timothy Lewis Amiano').first().click();
     await page.getByRole('textbox').nth(2).click();
-    var username_admin = 'Timothy'
-    var timestamp = Date.now().toString()
-    var result = username_admin.concat(timestamp);
+    // var username_admin = 'Timothy'
+    // var timestamp = Date.now().toString()
+    // var result = username_admin.concat(timestamp);
     await page.getByRole('textbox').nth(2).fill(result);
+    console.log(result)
     await page.getByRole('textbox').nth(3).click();
     await page.getByRole('textbox').nth(3).fill('password123');
     await page.getByRole('textbox').nth(4).click();
@@ -39,3 +69,19 @@ test('Add new admin user', async ({page}) => {
 
 });
 
+test('Edit existing admin user', async ({page}) => {
+
+    var search = ' '
+    var searchName = search.concat(result)
+    var full = searchName.concat(' ESS')
+    console.log(full)
+    
+    await page.getByRole('row', { name: full }).getByRole('button').nth(1).click();
+
+    await page.locator('form i').first().click();
+    await page.locator('form i').first().click();
+    await page.locator('form i').nth(1).click();
+    await page.getByRole('option', { name: 'Disabled' }).click();
+    await page.getByRole('button', { name: 'Save' }).click();
+
+})
